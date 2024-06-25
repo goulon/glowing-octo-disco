50467b2e873bdaa21eb873ceb8b627477505433c
Aurélien Goulon <a@goulon.net>
2024-06-24 19:57:18 -0600

    Add solution for async sorted merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,8 +1,37 @@
 "use strict";
+const MinHeap = require("../lib/min-heap");
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  await Promise.all(
+    logSources.map(async (logSource, index) => {
+      const lastEntry = await logSource.last;
+      if (lastEntry)
+        minHeap.insert(lastEntry.date, {
+          entry: lastEntry,
+          sourceIndex: index,
+        });
+    })
+  );
+
+  while (minHeap.size()) {
+    const {
+      payload: { entry, sourceIndex },
+    } = minHeap.extractMin();
+    printer.print(entry);
+    const nextEntry = await logSources[sourceIndex].pop();
+    if (nextEntry)
+      minHeap.insert(nextEntry.date, {
+        entry: nextEntry,
+        sourceIndex,
+      });
+  }
+
+  printer.done();
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
