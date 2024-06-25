b8346189ffd3a7f1d5e39a4d9860f4040942fb77
Aurélien Goulon <a@goulon.net>
2024-06-24 19:40:14 -0600

    Add solution for sync sorted merge

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,34 @@
 "use strict";
+const MinHeap = require("../lib/min-heap");
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  logSources.forEach((logSource, index) => {
+    const lastEntry = logSource.last;
+    if (lastEntry)
+      minHeap.insert(lastEntry.date, {
+        entry: lastEntry,
+        sourceIndex: index,
+      });
+  });
+
+  while (minHeap.size()) {
+    const {
+      payload: { entry, sourceIndex },
+    } = minHeap.extractMin();
+    printer.print(entry);
+    const nextEntry = logSources[sourceIndex].pop();
+    if (nextEntry)
+      minHeap.insert(nextEntry.date, {
+        entry: nextEntry,
+        sourceIndex,
+      });
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
